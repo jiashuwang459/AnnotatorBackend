a2c85765ab6bcadd48b43d6e1a24e3450f693626
Jiashu Wang <jiashuwang459@gmail.com>
2024-03-20 15:31:41 -0700

    temp

--- Annotator/settings.py
+++ Annotator/settings.py
@@ -30,7 +30,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y87-@d^e3gw=1*yf2-gjf#q2a5r-&d00d(ih6xb*mh%-v%lyp7'
+# SECRET_KEY = 'y87-@d^e3gw=1*yf2-gjf#q2a5r-&d00d(ih6xb*mh%-v%lyp7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
